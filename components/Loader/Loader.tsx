19515b32fd30e84f40b4ee8c63c9154802fe7940
sme12 <sazanovvs@gmail.com>
2023-10-26 23:42:51 +0300

    Add new loaders and optimize animation files

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -3,13 +3,15 @@ import Lottie from 'lottie-react';
 import loader1 from '@/public/loader1.json';
 import loader2 from '@/public/loader2.json';
 import loader3 from '@/public/loader3.json';
+import loader4 from '@/public/loader4.json';
+import loader5 from '@/public/loader5.json';
 
 const Loader: React.FC = () => {
-    const loaders = [loader1, loader2, loader3];
-    const animationData = loaders[Math.floor(Math.random() * 3)];
+    const loaders = [loader1, loader2, loader3, loader4, loader5];
+    const animationData = loaders[Math.floor(Math.random() * loaders.length)];
     return (
         <div
-            className="grow flex items-center justify-center w-full h-full"
+            className="flex h-full w-full grow items-center justify-center"
             data-testid="loader"
         >
             <Lottie
